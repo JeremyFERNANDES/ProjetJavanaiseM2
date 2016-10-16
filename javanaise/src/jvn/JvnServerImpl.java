60dbb88047019e06ee8c3797e774b27aef569976
AntoineThebaud <antoine.thebaud@yahoo.fr>
2016-10-16 19:13:04 +0200

    Ajout classe JavanaiseTest (/!\ erreur ajoutée)
    /!\ le deuxième test de saturation du cache ne marche plus..

--- javanaise/src/jvn/JvnServerImpl.java
+++ javanaise/src/jvn/JvnServerImpl.java
@@ -25,14 +25,14 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 	private Hashtable<Integer, JvnObject> cache;
 
 	private Hashtable<String, Integer> correspondanceNomId;
-	
-	private final int cacheMaxSize = 3; 
+
+	private final int cacheMaxSize = 3;
 
 	private JvnRemoteCoord coordinateur;
 
 	private JvnServerImpl() throws Exception {
 		super();
-		//System.out.println("JvnServerImpl.JvnServerImpl()");
+		// System.out.println("JvnServerImpl.JvnServerImpl()");
 		this.cache = new Hashtable<Integer, JvnObject>();
 		this.correspondanceNomId = new Hashtable<String, Integer>();
 		Registry registry = LocateRegistry.getRegistry("localhost");
@@ -40,7 +40,7 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 	}
 
 	public static JvnServerImpl jvnGetServer() {
-		//System.out.println("JvnServerImpl.jvnGetServer()");
+		// System.out.println("JvnServerImpl.jvnGetServer()");
 		if (js == null) {
 			try {
 				js = new JvnServerImpl();
@@ -52,12 +52,12 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 	}
 
 	public void jvnTerminate() throws jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnTerminate()");
+		// System.out.println("JvnServerImpl.jvnTerminate()");
 		// to be completed
 	}
 
 	public JvnObject jvnCreateObject(Serializable o) throws jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnCreateObject()");
+		// System.out.println("JvnServerImpl.jvnCreateObject()");
 		if (this.cache.size() == this.cacheMaxSize) {
 			suppressionElementCache();
 		}
@@ -66,18 +66,18 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 		try {
 			id = this.coordinateur.jvnGetObjectId();
 			o1 = new JvnObjectImpl(o, id, js);
-			synchronized(this) {
+			synchronized (this) {
 				this.cache.put(id, o1);
 			}
 		} catch (RemoteException e) {
 			e.printStackTrace();
-		}		
+		}
 		return this.cache.get(id);
 	}
 
 	public void jvnRegisterObject(String jon, JvnObject jo) throws jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnRegisterObject()");
-		synchronized(this) {
+		// System.out.println("JvnServerImpl.jvnRegisterObject()");
+		synchronized (this) {
 			this.correspondanceNomId.put(jon, jo.jvnGetObjectId());
 		}
 		try {
@@ -85,11 +85,11 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		afficheToiTchounibabe();
+		// rapport();
 	}
 
 	public JvnObject jvnLookupObject(String jon) throws jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnLookupObject()");
+		// System.out.println("JvnServerImpl.jvnLookupObject()");
 		// TODO : to be completed
 		if (this.cache.size() == this.cacheMaxSize) {
 			suppressionElementCache();
@@ -107,18 +107,18 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 			o = serveurObject;
 			o.setLockNL();
 			o.setServer(this);
-			synchronized(this) {
+			synchronized (this) {
 				this.correspondanceNomId.put(jon, o.jvnGetObjectId());
 				this.cache.put(o.jvnGetObjectId(), o);
 			}
 		}
-		//System.out.println(o.getVerrou());
-		afficheToiTchounibabe();
+		// System.out.println(o.getVerrou());
+		// rapport();
 		return o;
 	}
 
 	public Serializable jvnLockRead(int joi) throws JvnException {
-		//System.out.println("JvnServerImpl.jvnLockRead()");
+		// System.out.println("JvnServerImpl.jvnLockRead()");
 		Serializable s = null;
 		try {
 			s = this.coordinateur.jvnLockRead(joi, this.js);
@@ -129,7 +129,7 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 	}
 
 	public Serializable jvnLockWrite(int joi) throws JvnException {
-		//System.out.println("JvnServerImpl.jvnLockWrite()");
+		// System.out.println("JvnServerImpl.jvnLockWrite()");
 		Serializable s = null;
 		try {
 			s = this.coordinateur.jvnLockWrite(joi, this.js);
@@ -140,72 +140,79 @@ public class JvnServerImpl extends UnicastRemoteObject implements JvnLocalServer
 	}
 
 	public void jvnInvalidateReader(int joi) throws java.rmi.RemoteException, jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnInvalidateReader()");
+		// System.out.println("JvnServerImpl.jvnInvalidateReader()");
 		// TODO : to be completed
 		this.cache.get(joi).jvnInvalidateReader();
 	}
 
 	public Serializable jvnInvalidateWriter(int joi) throws java.rmi.RemoteException, jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnInvalidateWriter()");
+		// System.out.println("JvnServerImpl.jvnInvalidateWriter()");
 		// TODO : to be completed
 		return this.cache.get(joi).jvnInvalidateWriter();
 	}
 
 	public Serializable jvnInvalidateWriterForReader(int joi) throws java.rmi.RemoteException, jvn.JvnException {
-		//System.out.println("JvnServerImpl.jvnInvalidateWriter()");
+		// System.out.println("JvnServerImpl.jvnInvalidateWriter()");
 		// TODO : to be completed
 		return this.cache.get(joi).jvnInvalidateWriterForReader();
 	}
-	
+
 	public void suppressionElementCache() throws JvnException {
 		Set cles = this.cache.keySet();
 		Iterator it = cles.iterator();
 		int idObjASupprimmer = -1;
-		
+
 		lockStates currentLock;
 		lockStates toRemoveLock;
-		while (it.hasNext()){
-		   Object cle = it.next();
-		   currentLock = this.cache.get(cle).getVerrou();
-		   toRemoveLock = idObjASupprimmer != -1 ? this.cache.get(idObjASupprimmer).getVerrou() : null;
-		   
-		   if ( (currentLock == lockStates.NL || currentLock == lockStates.RC || currentLock == lockStates.WC) &&  idObjASupprimmer == -1 ){
-			   idObjASupprimmer = (Integer) cle;
-		   } else if ( currentLock == lockStates.NL && (toRemoveLock == lockStates.RC || toRemoveLock == lockStates.WC) ){
-			   idObjASupprimmer = (Integer) cle;
-		   } else if ( currentLock == lockStates.NL && toRemoveLock == lockStates.NL && this.cache.get(cle).getTimeUnlock().before(this.cache.get(idObjASupprimmer).getTimeUnlock()) ){
-			   idObjASupprimmer = (Integer) cle;
-		   } else if ( (currentLock == lockStates.WC || currentLock == lockStates.RC) && 
-				   (toRemoveLock == lockStates.WC || toRemoveLock == lockStates.RC) &&
-				   this.cache.get(cle).getTimeUnlock().before(this.cache.get(idObjASupprimmer).getTimeUnlock()) ){
-			   idObjASupprimmer = (Integer) cle;
-		   }
-		}
-		   
-	   if (idObjASupprimmer == -1) {
-		   throw new JvnException("Impossible de trouver de la place");
-	   }
-	   else {
-		   if( this.cache.get(idObjASupprimmer).getVerrou() == lockStates.WC || this.cache.get(idObjASupprimmer).getVerrou() == lockStates.RC) {
-			   try {
-				this.coordinateur.jvnObjectDeleteToTheChache(js, idObjASupprimmer);
-			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		while (it.hasNext()) {
+			Object cle = it.next();
+			currentLock = this.cache.get(cle).getVerrou();
+			toRemoveLock = idObjASupprimmer != -1 ? this.cache.get(idObjASupprimmer).getVerrou() : null;
+
+			if ((currentLock == lockStates.NL || currentLock == lockStates.RC || currentLock == lockStates.WC)
+					&& idObjASupprimmer == -1) {
+				idObjASupprimmer = (Integer) cle;
+			} else if (currentLock == lockStates.NL
+					&& (toRemoveLock == lockStates.RC || toRemoveLock == lockStates.WC)) {
+				idObjASupprimmer = (Integer) cle;
+			} else if (currentLock == lockStates.NL && toRemoveLock == lockStates.NL
+					&& this.cache.get(cle).getTimeUnlock().before(this.cache.get(idObjASupprimmer).getTimeUnlock())) {
+				idObjASupprimmer = (Integer) cle;
+			} else if ((currentLock == lockStates.WC || currentLock == lockStates.RC)
+					&& (toRemoveLock == lockStates.WC || toRemoveLock == lockStates.RC)
+					&& this.cache.get(cle).getTimeUnlock().before(this.cache.get(idObjASupprimmer).getTimeUnlock())) {
+				idObjASupprimmer = (Integer) cle;
 			}
-		   }
+		}
+
+		if (idObjASupprimmer == -1) {
+			throw new JvnException("Impossible de trouver de la place");
+		} else {
+			if (this.cache.get(idObjASupprimmer).getVerrou() == lockStates.WC
+					|| this.cache.get(idObjASupprimmer).getVerrou() == lockStates.RC) {
+				try {
+					this.coordinateur.jvnObjectDeleteToTheChache(js, idObjASupprimmer);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			this.cache.remove(idObjASupprimmer);
 			Set cles2 = this.correspondanceNomId.keySet();
 			Iterator it2 = cles2.iterator();
-			while (it2.hasNext()){
-			   Object cle = it2.next();
-			   if ( this.correspondanceNomId.get(cle) == idObjASupprimmer )
-				   it2.remove();
-		   }
-	   }
+			while (it2.hasNext()) {
+				Object cle = it2.next();
+				if (this.correspondanceNomId.get(cle) == idObjASupprimmer)
+					it2.remove();
+			}
+		}
 	}
-	
-	public void afficheToiTchounibabe() {
+
+	public Hashtable<String, Integer> getHashmap() {
+		return new Hashtable<String, Integer>(correspondanceNomId);
+	}
+
+	public void rapport() {
 		System.out.println("cache :");
 		System.out.println(cache);
 		System.out.println("correspondanceNomId :");
---
+++ javanaise/src/test/JavanaiseTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Set;
+
+import org.junit.Test;
+
+import irc.Sentence;
+import irc.SentenceInterface;
+import jvn.JvnProxy;
+import jvn.JvnServerImpl;
+
+public class JavanaiseTest {
+
+	@Test
+	public final void testSaturation1() {
+		try {
+			ArrayList<SentenceInterface> ar = new ArrayList<SentenceInterface>();
+			for(int i = 0; i <= 3; ++i) {
+				ar.add((SentenceInterface) JvnProxy.newInstance("tS1_"+i, Sentence.class));
+			}
+			String obtainedOutput = JvnServerImpl.jvnGetServer().getHashmap().keySet().toString();
+			String expectedOutput = "[tS1_3, tS1_2, tS1_1]";
+			assertEquals(obtainedOutput, expectedOutput);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public final void testSaturation2() {
+		try {
+			ArrayList<SentenceInterface> ar = new ArrayList<SentenceInterface>();
+			for(int i = 0; i <= 3; ++i) {
+				ar.add((SentenceInterface) JvnProxy.newInstance("tS2_"+i, Sentence.class));
+				if(i == 0)
+					ar.get(0).read();
+				if(i == 1)
+					ar.get(1).write("text");
+			}
+			String obtainedOutput = JvnServerImpl.jvnGetServer().getHashmap().keySet().toString();
+			String expectedOutput = "[tS2_3, tS2_1, tS2_0]";
+			assertEquals(obtainedOutput, expectedOutput);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
